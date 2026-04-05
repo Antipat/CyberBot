04fe3c401245c953140240b21953e190f54308be
Antipat <49370907+Antipat@users.noreply.github.com>
2026-04-05 11:29:47 +0300

    Библиотека Matrix для дисплея 16x8

---
+++ Matrix/Matrix.d
@@ -0,0 +1,13 @@
+C:\Users\Win10_Game_OS\mblock-avr\temp\build\Matrix\Matrix.o: \
+ C:\Users\Win10_Game_OS\mblock-avr\temp\build\Matrix\Matrix.cpp \
+ C:\Users\Win10_Game_OS\mblock-avr\temp\build\Matrix\Matrix.h \
+ avr-library/cores/arduino/Arduino.h avr-library/cores/arduino/binary.h \
+ avr-library/cores/arduino/WCharacter.h \
+ avr-library/cores/arduino/WString.h \
+ avr-library/cores/arduino/HardwareSerial.h \
+ avr-library/cores/arduino/Stream.h avr-library/cores/arduino/Print.h \
+ avr-library/cores/arduino/Printable.h avr-library/cores/arduino/USBAPI.h \
+ avr-library/cores/arduino/Arduino.h \
+ C:\Users\Public\Programs\mblock\resources\ml\v1\external\arduino\avr-library\variants\standard/pins_arduino.h \
+ avr-library/cores/arduino/Print.h \
+ C:\Users\Win10_Game_OS\mblock-avr\temp\build\Matrix\glcdfont.c
---
+++ Matrix/glcdfont.d
@@ -0,0 +1,2 @@
+C:\Users\Win10_Game_OS\mblock-avr\temp\build\Matrix\glcdfont.o: \
+ C:\Users\Win10_Game_OS\mblock-avr\temp\build\Matrix\glcdfont.c
